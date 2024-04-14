55884c1f3fdad3dc90249176842a71fe1465216d
soyUnaGata <fallen.snitch@gmail.com>
2024-04-14 21:07:50 +0300

    implement checking of email

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -24,7 +24,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => 'required|string|min:3|max:50',
-            'email' => 'required|string|email|unique:users,email',
+            'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed',
         ];
     }
