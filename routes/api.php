f97b40b332c834a8e27cb4019732f216584f5ff7
soyUnaGata <fallen.snitch@gmail.com>
2024-04-14 21:07:01 +0300

    implement reg, login routes

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,25 @@
 <?php
+namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\CompleteTaskController;
+use \App\Http\Controllers\Api\Auth\LoginController;
+use \App\Http\Controllers\Api\Auth\LogoutController;
+use \App\Http\Controllers\Api\Auth\RegisterController;
 
-Route::prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
-Route::get('/user', function (Request $request) {
+Route::prefix('auth')->group(function(){
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('register', RegisterController::class);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
