1693d573dc6803d80fd7a7c5026924bce5ec0450
soyUnaGata <fallen.snitch@gmail.com>
2024-04-16 16:42:08 +0300

    implement new tasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Task;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Task::factory(10)->create();
+        User::factory(5)->has(
+            Task::factory(10)
+        )->create();
+
     }
 }
